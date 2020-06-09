87e969605326d521ffe8d8faee1c1432b993736b
Mihail Nojakshev <Nojakschev@mail.ru>
2020-06-09 18:02:03 +0500

    Загрузил сложное дз к 3 уроку

--- hardlesson/script.js
+++ hardlesson/script.js
@@ -1,18 +1,73 @@
-﻿let num = '266219';
-let summ;
-let iMax = num.length;
+﻿"use strict";
+let lan = prompt("Введите ru или en", "ru");
+
+console.log("Через if");
+if (lan === "ru") {
+    console.log("Понедельник");
+    console.log("Вторник");
+    console.log("Среда");
+    console.log("Четверг");
+    console.log("Пятница");
+    console.log("Суббота");
+    console.log("Воскресенье");
+} else {
+    console.log("Monday");
+    console.log("Tuesday");
+    console.log("Wednesday");
+    console.log("Thursday");
+    console.log("Friday");
+    console.log("Saturday");
+    console.log("Sunday");
+}
+console.log("Через switch-case");
+
+switch (lan) {
+    case "ru":
+        console.log("Понедельник");
+        console.log("Вторник");
+        console.log("Среда");
+        console.log("Четверг");
+        console.log("Пятница");
+        console.log("Суббота");
+        console.log("Воскресенье");
+        break;
+    case "en":
+        console.log("Monday");
+        console.log("Tuesday");
+        console.log("Wednesday");
+        console.log("Thursday");
+        console.log("Friday");
+        console.log("Saturday");
+        console.log("Sunday");
+        break;
+}
+console.log("Через многомерный массив");
 let arr = [];
-for (var i = 0; i < iMax; i++) {
-    arr[i] = num.charAt(i);
+arr["ru"] = [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+];
+arr["en"] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+console.log(arr[lan]);
 
-}
-console.log(arr);
-summ = 1;
-for (var i = 0; i < iMax; i++) {
-    summ = summ*arr[i];
-
-}
-console.log(summ);
-summ = String(summ**3);
-console.log(summ);
-console.log(summ.substring(0, 2));
+let namePerson = prompt("Введите имя");
+let result =
+    namePerson === "Артем" ?
+    "Директор" :
+    namePerson === "Максим" ?
+    "Преподаватель" :
+    "студент";
+console.log(result);
