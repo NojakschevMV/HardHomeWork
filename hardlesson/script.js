a0f0874d54a9e27f8483277b8b54f35b376c0f24
Mihail Nojakshev <Nojakschev@mail.ru>
2020-06-11 17:24:40 +0500

    Загрузил сложное дз к уроку 5

---
+++ hardlesson/script.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+function editLine(str) {
+  if (typeof str === "string") {
+    var str2 = str.trim();
+    if (str2.length > 30) {
+      console.log(str2.substr(0, 30) + "...");
+    }
+  } else {
+    console.log("В качестве аргумента передана не строка");
+  }
+}
+//editLine('Bheflerkgmdflkgdfngndfjbndflknblfdnblknfdbndskfbgkdfkndfknbkdfnbkdsfkbnsdkfnbkdnbksdfkbjsdfb');
+//editLine(55);
+//editLine(true);
